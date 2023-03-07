d532998ca2683ecc8fc03a01f986fe92dab96c8d
Ian Holender <a01026295@tec.mx>
2023-03-07 16:34:21 -0600

    fixing web page

---
+++ base datos /prueba.sql
@@ -0,0 +1,7 @@
+drop schema IF EXISTS gearsecond;
+Create schema gearsecond;
+Use gearsecond;
+CREATE TABLE prueba(
+idPrueba int primary key not null, 
+nombre Varchar(30),
+password Varchar (30));
