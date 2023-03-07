42d2c709b0dd9cc2a236fb58c327f8c6a25481d2
Ian Holender <a01026295@tec.mx>
2023-03-07 12:36:24 -0600

    cs

--- juegos/PrimerJuego/scripts /ball-in.cs
+++ juegos/PrimerJuego/scripts /ball-in.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Balll_in : MonoBehaviour
+public class Ball_in : MonoBehaviour
 {
     [SerializeField] Score scoreObj;
     // Start is called before the first frame update
