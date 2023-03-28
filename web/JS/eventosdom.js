ab2d56ca5916a8beaba06c1627b20bb5f9886f11
Ian Holender <a01026295@tec.mx>
2023-03-28 11:54:17 -0600

    avance chatgpt y dom

---
+++ web/JS/eventosdom.js
@@ -0,0 +1,8 @@
+// q: que es el DOM?
+// a: es la representacion de un documento html en forma de arbol
+
+// q: que es un nodo?
+// a: es un elemento del arbol
+
+// q: como se accede a un elemento del DOM?
+// a: document.getElementById("id")
