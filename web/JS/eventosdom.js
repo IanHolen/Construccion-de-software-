e8cd0afb4285f7098a161176c1af332fb78c78a7
Ian Holender <a01026295@tec.mx>
2023-03-29 14:02:32 -0600

    DOM Final

--- web/JS/eventosdom.js
+++ web/JS/eventosdom.js
@@ -69,27 +69,42 @@ changeBtn.addEventListener("click", function() {
 
 //Ejercicio 5
 //
-const colorSelect = document.getElementById("colorSelect");
-const addColorBtn = document.getElementById("btn-add-color");
-const removeColorBtn = document.getElementById("btn-rmv-color");
+// Obtener referencias a los elementos del DOM
+const colorSelect = document.getElementById('colorSelect');
+const btnAddColor = document.getElementById('btn-add-color');
+const btnRmvColor = document.getElementById('btn-rmv-color');
 
-addColorBtn.addEventListener("click", function() {
-  const newColor = document.createElement("option");
-  newColor.text = getRandomColor();
-  colorSelect.add(newColor);
-});
+// Función para agregar un color a la lista de opciones
+function addColor() {
+  // Crear un nuevo elemento de opción
+  const newOption = document.createElement('option');
+  // Generar un color aleatorio
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  // Establecer el valor y el texto del nuevo elemento de opción
+  newOption.value = randomColor;
+  newOption.textContent = randomColor;
+  // Agregar el nuevo elemento de opción a la lista de opciones
+  colorSelect.appendChild(newOption);
+}
 
-removeColor
+// Función para eliminar el último color de la lista de opciones
+function removeColor() {
+  // Obtener la cantidad de opciones en la lista
+  const optionCount = colorSelect.options.length;
+  // Si hay al menos dos opciones, eliminar la última opción
+  if (optionCount >= 2) {
+    colorSelect.removeChild(colorSelect.options[optionCount - 1]);
+  }
+}
 
-//Ejercicio 6
-//
-const imagenGato = document.getElementById("imagenGato");
+// Agregar controladores de eventos a los botones
+btnAddColor.addEventListener('click', addColor);
+btnRmvColor.addEventListener('click', removeColor);
 
-imagenGato.addEventListener("mouseenter", () => {
-  const minWidth = 300;
-  const maxWidth = 600;
-  const width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-  const height = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-  const newSrc = `http://placekitten.com/${width}/${height}`;
-  imagenGato.src = newSrc;
+//Ejercicio 6 
+// 
+document.getElementById("imagenGato").addEventListener('mouseenter', function () {
+  const width = Math.floor(Math.random() * (600 - 300 + 1)) + 300;
+  const height = Math.floor(Math.random() * (600 - 300 + 1)) + 300;
+  this.src = `http://placekitten.com/${width}/${height}`;
 });
